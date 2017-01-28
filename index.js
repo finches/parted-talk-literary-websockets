b05e25090d80033bff00290cc1f50dfbfe1b169a
Ryan Hammond <ryanhammond13@gmail.com>
2017-01-28 08:26:57 -0800

    Set up initial connection

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 http.listen(4001, function(){
 	  console.log('listening on *:4001');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
