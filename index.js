a6084aad00dec5d9a17aaae3c93a609a83150081
Ryan Hammond <ryanhammond13@gmail.com>
2017-01-29 08:51:56 -0800

    set up initial connection hooks for the ui

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ http.listen(4001, function(){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  socket.on('user-name', function(name){
+    console.log('user-name: ' + name);
+  });
+
+  socket.on('new-line', function(line){
+    console.log('message: ' + line);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
